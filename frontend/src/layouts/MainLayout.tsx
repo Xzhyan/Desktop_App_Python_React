8784d385df4be08207b848e7b07ace52dcb705e2
prxblms <malcondev.lds@hotmail.com>
2026-06-08 12:34:58 -0300

    montando o home

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,17 +3,19 @@ import { Outlet } from "react-router-dom";
 export default function MainLayout() {
     return (
         <>
-            <header>
-                Desktop Test App
+            <header className="bg-green-950 text-center p-2 shadow-md">
+                <h1>Desktop Test App</h1>
             </header>
 
-            <main>
+            <nav>
+                <ul>
+                    <a href=""></a>
+                </ul>
+            </nav>
+
+            <main className="flex justify-center">
                 <Outlet />
             </main>
-
-            <footer>
-                Rodapé
-            </footer>
         </>
     )
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react"
+
+// api
+import api from "../routes/api";
+
+
+interface Animal {
+    id: number;
+    identification: string;
+}
+
 
 export default function HomePage() {
+    const [animals, setAnimals] = useState<Animal[]>([]);
+
+    useEffect(() => {
+        async function getAnimals() {
+            try {
+                const response = await api.get<Animal[]>('/animals');
+
+                setAnimals(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getAnimals();
+    }, []);
+
     return (
-        <></>
+        <div className="flex w-6xl bg-green-900 p-2 shadow-md">
+            <ul>
+                {animals.map((animal) => (
+                    <li key={animal.id}>
+                        {animal.identification}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
